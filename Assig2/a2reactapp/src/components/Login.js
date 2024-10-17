5884fe4ec3c28540d1585bf2a59dd233bba5e500
davgy016 <133870761+davgy016@users.noreply.github.com>
2024-10-17 20:17:19 +1030

    This is incomplete code ! ! !Added some code in Login.
     At this moment if username exists and password match then login is successful. But user can also  log in  without credentials or only with password.
    code needs improvement, also need to return some data after login

--- Assig2/a2reactapp/src/components/Login.js
+++ Assig2/a2reactapp/src/components/Login.js
@@ -3,10 +3,51 @@ import SHA256 from 'crypto-js/sha256';
 
 
 
-function Login() {   
+function Login() {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    function onSubmit(e) {
+        e.preventDefault();
+
+        //extract input values from Form
+        const form = e.target;
+        const formData = new FormData(form);
+        const userName = formData.get("userName");
+        const password = formData.get("password");
+
+        //encrypt the password
+        const passwordHash = SHA256(password).toString();
+
+        /**Making post request to send credentials for a log in
+         * Ref: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch 
+         * 
+         */
+        var request = new Request(`http://localhost:5147/api/Login?userName=${userName}&passwordHash=${passwordHash}`, {
+            method: 'Post',
+            headers: {
+                "Content-Type": "Application/json",
+            },
+            body: JSON.stringify({ userName, password: passwordHash }),
+        });
+
+        //checks validation of provided credentials
+        fetch(request)
+            .then(response => response.json())
+            .then(data => {
+                if (data) {
+                    setIsLoggedIn(true);
+                    alert("Login successful");
+                } else {
+                    alert("Login failed");
+                }
+            })
+            .catch(error => console.error("Error" + error));
+
+    }
 
     return (
-      
+        <form onSubmit={onSubmit} className="row justify-content-start mb-3">
             <div className="row">
                 <div className="col-3">
                     <input type="text" name="userName" className="form-control"
@@ -20,7 +61,7 @@ function Login() {
                     <button type="submit" className="form-control btn btn-primary">Login</button>
                 </div>
             </div>
-      
+        </form>
     )
 }
 
