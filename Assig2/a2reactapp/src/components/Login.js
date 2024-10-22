925c061263f5bea48b04dab4e53f90753b16bdf6
davgy016 <133870761+davgy016@users.noreply.github.com>
2024-10-22 15:28:24 +1030

    Modified Login - added code for registration and it allows to use same page either login or register

--- Assig2/a2reactapp/src/components/Login.js
+++ Assig2/a2reactapp/src/components/Login.js
@@ -9,8 +9,11 @@ import { FaLock, FaUser } from "react-icons/fa";
 function Login() {
 
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isRegistering, setIsRegistering] = useState(false);
     const navigate = useNavigate();
     const { setUser } = useOutletContext();
+
+
     function onSubmit(e) {
         e.preventDefault();
 
@@ -22,6 +25,15 @@ function Login() {
 
         //encrypt the password
         const passwordHash = SHA256(password).toString();
+
+        if (isRegistering) {
+            registerUser(userName, passwordHash);
+        } else {
+            loginUser(userName, passwordHash);
+        }
+    }
+
+    function loginUser(userName, passwordHash) {
 
         /**Making post request to send credentials for a log in
          * Ref: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch 
@@ -43,8 +55,9 @@ function Login() {
                 if (data == true) {
                     setIsLoggedIn(true);
                     setUser(userName);
-                   
-                    //After successful Login redirects to the Home pagehttps://reactrouter.com/en/main/hooks/use-navigate 
+
+                    //After successful Login redirects to the Home page 
+                    //https://reactrouter.com/en/main/hooks/use-navigate 
                     navigate("/Home");
                 } else {
                     alert("Login failed");
@@ -54,10 +67,38 @@ function Login() {
 
     }
 
+    function registerUser(userName, passwordHash) {
+
+        //Send post request to register API         
+        var request = new Request(`http://localhost:5147/api/Register?userName=${userName}&passwordHash=${passwordHash}`, {
+            method: 'Post',
+            headers: {
+                "Content-Type": "Application/json",
+            },
+            body: JSON.stringify({ userName, password: passwordHash }),
+        });
+
+        //checks validation of provided credentials
+        fetch(request)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data); // Check the response here
+                if (data == true) {
+                    setIsRegistering(true);  
+                    alert("Successfully registered")
+                    navigate("/Home");
+                } else {
+                    alert("Cannot register, userName already exists");
+                }
+            })
+            .catch(error => console.error("Error" + error));
+
+    }
+
     return (
-        <div className="wrapper">   
+        <div className="wrapper">
             <form onSubmit={onSubmit}>
-                <h1>Please Login</h1>
+                <h1>{ isRegistering ? "Please Register" : "Please Login"}</h1>
 
                 <div className="input-box">
                     <input type="text" name="userName"
@@ -80,7 +121,7 @@ function Login() {
                 <button type="submit">Login</button>
 
                 <div className="register-link">
-                    <p>Don't have an account? <a href="#">Register</a></p>
+                    <p>Don't have an account? <a href="#" onClick={() => setIsRegistering(true) }>Register</a></p>
                 </div>
             </form>
         </div>
