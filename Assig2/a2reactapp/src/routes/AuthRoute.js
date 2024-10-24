51efe9a3680d648e1c26cc615a96684d0a743089
davgy016 <133870761+davgy016@users.noreply.github.com>
2024-10-24 12:18:43 +1030

    Created class for authentication

---
+++ Assig2/a2reactapp/src/routes/AuthRoute.js
@@ -0,0 +1,12 @@
+﻿import { useOutletContext, Navigate } from 'react-router-dom'
+
+function AuthRoute({ route }) {
+    const { user } = useOutletContext();
+
+    if (!user) {
+        return <Navigate to="/Login"></Navigate>;
+    }
+    return route;
+}
+
+export default AuthRoute;
