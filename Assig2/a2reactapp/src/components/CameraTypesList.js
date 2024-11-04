6de19b651c05c3e3aaf75af140088113737a1ddb
davgy016 <133870761+davgy016@users.noreply.github.com>
2024-11-04 17:33:34 +1030

    Created CameraTypesList  for camera type select list  in Dashboard

---
+++ Assig2/a2reactapp/src/components/CameraTypesList.js
@@ -0,0 +1,30 @@
+﻿import React, { useEffect, useState } from 'react';
+
+function CameraTypeList({ onSelectCameraType } ) {
+
+    const cameraTypesCodeList = ["M", "PAC", "I/section", "P2P", "Rail", "Mid Block"];
+    const [selectedCameraType, setSelectedCameraType] = useState("");
+
+    const selectCameraType = (e) => {
+        const cameraType = e.target.value;
+        setSelectedCameraType(cameraType);
+        onSelectCameraType(cameraType);
+    }
+
+
+    return (
+        <div className="CameraTypesList">
+            <select className="form-select" value={selectedCameraType} onChange={selectCameraType}>
+                {/*enable to select suburb from suburblist */}
+                <option value="">Select Camera Type</option>
+                {cameraTypesCodeList.map((cameraType, index) => (
+                    <option key={index} value={cameraType}>
+                        {cameraType}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+}
+
+export default CameraTypeList;
