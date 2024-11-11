15cdf49543903c97674e827645f2d20d56a7c489
davgy016 <133870761+davgy016@users.noreply.github.com>
2024-11-11 17:19:35 +1030

    Created card. Later will be used to display  some details in Report page

---
+++ Assig2/a2reactapp/src/components/Card.js
@@ -0,0 +1,22 @@
+﻿
+
+const Card = ({ locationId, totalFee, totalDemerits, avgFeeDaily, avgDemeritsDaily }) => {   
+
+    return (
+        <div className="card mb-3" style={{ width: '500px', height: '600px' }}>
+            <div className="row">
+                <div>
+                    <div className="card-body">
+                        <h5 className="card-title">LocationID: {locationId}</h5>
+                        <p className="card-text">Total Fee: {totalFee}</p>
+                        <p className="card-text">Total Demerits: {totalDemerits}</p>
+                        <p className="card-text">Avg. Daily Fee: {avgFeeDaily}</p>
+                        <p className="card-text">Avg. Daily Demerits: {avgDemeritsDaily}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
