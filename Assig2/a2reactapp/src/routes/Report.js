ecaaddb32cab521eb905f05202c8e2bb90f779d1
davgy016 <133870761+davgy016@users.noreply.github.com>
2024-11-11 17:31:03 +1030

    pin point 2 selected  locations  on the map  in Report page

--- Assig2/a2reactapp/src/routes/Report.js
+++ Assig2/a2reactapp/src/routes/Report.js
@@ -1,4 +1,9 @@
-﻿const Report = ({ }) => {
+﻿import Card from '../components/Card';
+import React from 'react';
+
+
+function Report({ }) {
+
     return (
         <div>
             <h2 className="text-center">
@@ -6,6 +11,30 @@
             </h2>
             <hr />
             <p>It was made for Report testing</p>
+            <div className="row gap-3 p-4">
+                <Card className="col-4"
+                    key={10}
+                    locationId={118}
+                    suburbName={"Adelaide"}
+                    roadName={"Grote Street/West Terrace"}
+                    latitude={-34.929203}
+                    longitude={138.587725}
+                    totalFee={1}
+                    totalDemerits={2}
+                    avgDemeritsDaily={3}
+                />
+                <Card
+                    key={20}
+                    locationId={170}
+                    suburbName={"Clovelly Park"}
+                    roadName={"South Road"}
+                    latitude={-34.993378}
+                    longitude={138.574858}
+                    totalFee={1}
+                    totalDemerits={2}
+                    avgDemeritsDaily={3}
+                />
+            </div>  
         </div>
     )
 }
